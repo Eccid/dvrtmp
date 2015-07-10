21c19850d7ffdf2d651469cdf32e28368a69230a
Stephen Yeager <syeager@mrc.org>
2015-07-10 09:57:19 -0400

    Initial

---
+++ js/mrc.js
@@ -0,0 +1,15 @@
+/*var secondNav = $("#second-nav");
+$("#search-toggle").click(function (){
+	if (secondNav.hasClass("hidden")) {
+		//$(document.body).animate({paddingTop : "20"}, 200);
+		secondNav.removeClass("hidden").addClass("animated slideInDown");
+		if (secondNav.hasClass("slideOutUp")) {
+			secondNav.removeClass("slideOutUp");
+		}
+	} else {
+		secondNav.removeClass("slideInDown").addClass("slideOutUp");
+		setTimeout(function () {
+			secondNav.addClass("hidden");
+		}, 700)
+	};
+})*/
